f9c8b5758194ca1fa315956b9ad30c56cc0d94d6
Lucas <lucasfsouza.l10@gmail.com>
2023-12-13 20:28:53 -0300

    [CONFIG] Alterando configurações do nextjs

--- next.config.js
+++ next.config.js
@@ -2,18 +2,18 @@
 //  * @type {import('next').NextConfig}
 //  */
 
-// const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === 'production';
 
 
 const withNextIntl = require('next-intl/plugin')();
  
 module.exports = withNextIntl({
     // assetPrefix: '/[lang]/',
-    // basePath:isProd ? '/website-nk': '',
-    // output: 'export',
-    // distDir: 'dist',
+    basePath:isProd ? '/website-nk': '',
+    output: 'export',
+    distDir: 'dist',
     // basePath:'',
-    // images: {
-    //     unoptimized: true,
-    // }
+    images: {
+        unoptimized: true,
+    }
 });
