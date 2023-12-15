4b50059ff357c21c5a89dd37aff404c88c4991cb
Lucas <lucasfsouza.l10@gmail.com>
2023-12-15 15:59:24 -0300

    [SECTION] Implementando sessão Shop

--- app/[locale]/components/sections/SectionShop.tsx
+++ app/[locale]/components/sections/SectionShop.tsx
@@ -1,6 +1,17 @@
+'use client'
+//intl
+import { useTranslations } from "next-intl";
 
 const SectionShop = () => {
-  return <div className="w-full h-[100vh] bg-slate-50">Shop</div>;
+  const t = useTranslations("Shop");
+
+  return (
+    <div className="w-full h-[100vh]">
+      <h2 className="text-4xl uppercase w-full h-[200px] flex justify-center items-center">
+        {t("title")}
+      </h2>
+    </div>
+  );
 };
 
 export default SectionShop;
